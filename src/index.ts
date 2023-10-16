e51c7101e424010da06224b575753a79686a288c
Nico <zygos.nico@gmail.com>
2023-10-16 07:41:00 +0100

    add file loaders

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("app is running")
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+// Loaders
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { JSONLoader } from "langchain/document_loaders/fs/json";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+import { config } from "dotenv";
+import { Document } from "langchain/document";
+
+config();
+
+// Read data useing directory loader
+const loader = new DirectoryLoader("./docs", {
+	".json": (path) => new JSONLoader(path),
+	".txt": (path) => new TextLoader(path),
+	".csv": (path) => new CSVLoader(path, { separator: "," }),
+	".pdf": (path) => new PDFLoader(path),
+});
+
